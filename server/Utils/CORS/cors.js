47e572d0fc2f1354fa79ac40a993b4f9791eec74
jugalnoob <sjugal126@gmnail.com>
2026-08-31 18:52:07 +0530

    newchnage

--- server/Utils/CORS/cors.js
+++ server/Utils/CORS/cors.js
@@ -1,6 +1,5 @@
-// 1. CORS Configuration
- export const corsOptions = {
-  origin: "https://frontendmain-git-master-jugalnoobs-projects.vercel.app/",
+export const corsOptions = {
+  origin: "https://frontendmain-git-master-jugalnoobs-projects.vercel.app",
   credentials: true,
-  methods: "GET,POST,PUT,DELETE,PATCH,HEAD",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
 };
