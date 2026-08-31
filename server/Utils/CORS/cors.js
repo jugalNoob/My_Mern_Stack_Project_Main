7944a75c1d06e4c04eefd3fbe0886022aaa3d9fe
jugalnoob <sjugal126@gmnail.com>
2026-08-31 10:39:35 +0530

    five

--- server/Utils/CORS/cors.js
+++ server/Utils/CORS/cors.js
@@ -1,6 +1,6 @@
 // 1. CORS Configuration
  export const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "http://localhost:5173",
   credentials: true,
   methods: "GET,POST,PUT,DELETE,PATCH,HEAD",
 };
